933b7653f5b7ccd03c0eff3a7af0b89e73899e40
mohsenjalalian <jalalianmohsen@gmail.com>
2017-10-02 06:28:19 +0330

    datepicker

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=countdown',
     'username' => 'root',
-    'password' => '1364@1367qweR',
+    'password' => '',
     'charset' => 'utf8',
 ];
